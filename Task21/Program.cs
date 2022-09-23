2705a3c9eb1c38bc33953a626a3b0d9ab8328de7
Svetlana Lap <laptevas80@gmail.com>
2022-09-23 21:31:16 +0300

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Random random = new Random();
+int x1 = random.Next(-100, 100);
+int x2 = random.Next(-100, 100);
+int y1 = random.Next(-100, 100);
+int y2 = random.Next(-100, 100);
+int z1 = random.Next(-100, 100);
+int z2 = random.Next(-100, 100);
+double dist;
+dist = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+Console.WriteLine(dist);
